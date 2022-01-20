875649192e42f850308a42822e796a870f4c63dd
Ostap Sulyk <ostapsulyk@gmail.com>
2022-01-20 18:37:39 -0500

    refactored selection sort to be more readable and added selection_sort_desc

--- simple_algorithms/src/main.rs
+++ simple_algorithms/src/main.rs
@@ -1,18 +1,25 @@
 #![allow(unused_imports)]
 mod un_ordered_array;
+use std::time::{Duration, Instant};
+
 use rand::prelude::*;
+
 use un_ordered_array::UnOrderedArray;
 
 fn main() {
     let mut ua: UnOrderedArray = UnOrderedArray::new();
     let mut rgn = thread_rng();
 
-    for _ in 0..10{
+    for _ in 0..10 {
         ua.add_last(rgn.gen_range(0..100))
     }
 
-    println!("{}", &ua.to_string());
+    // let now = Instant::now();
+    println!("{}", ua.list_items());
     ua.selection_sort();
+    println!("{}", ua.list_items());
+    ua.selection_sort_desc();
+    println!("{}", ua.list_items());
 
-    println!("{}", &ua.to_string());
+    // println!("{}", now.elapsed().as_secs());
 }
--- simple_algorithms/src/un_ordered_array.rs
+++ simple_algorithms/src/un_ordered_array.rs
@@ -1,23 +1,24 @@
 #![allow(dead_code, unused_variables)]
 
 pub struct UnOrderedArray {
-    m_array: Vec<i32>,
+    list: Vec<i32>,
 }
 
 impl UnOrderedArray {
     pub fn new() -> UnOrderedArray {
-        UnOrderedArray {
-            m_array: Vec::new(),
-        }
+        UnOrderedArray { list: Vec::new() }
     }
 
     pub fn add_last(&mut self, item: i32) {
-        self.m_array.push(item)
+        self.list.push(item)
+    }
+    pub fn remove_last(&mut self) {
+        self.list.pop();
     }
 
     pub fn linear_search(&self, item: i32) -> i32 {
-        for i in 0..self.m_array.len() {
-            if self.m_array[i] == item {
+        for i in 0..self.list.len() {
+            if self.list[i] == item {
                 return i as i32;
             }
         }
@@ -25,25 +26,45 @@ impl UnOrderedArray {
     }
 
     pub fn selection_sort(&mut self) {
-        let end = self.m_array.len();
-
-        for start in 0..end {
-            let index_of_min = start;
-            for i in start..end {
-                if self.m_array[i] < self.m_array[index_of_min] {
-                    let temp = self.m_array[index_of_min];
-                    self.m_array[index_of_min] = self.m_array[i];
-                    self.m_array[i] = temp;
+        let end = self.list.len();
+        for first_unsorted in 0..end {
+            // assume firs element in array is smallest
+            let mut location_of_smallest = first_unsorted;
+            for i in first_unsorted..end {
+                if self.list[location_of_smallest] > self.list[i] {
+                    location_of_smallest = i;
                 }
             }
+            // once we iterated to the end of the list
+            // 1 create temp variable
+            let temp = self.list[first_unsorted];
+            // 2 swap smallest element with first unsorted element
+            self.list[first_unsorted] = self.list[location_of_smallest];
+            self.list[location_of_smallest] = temp;
         }
     }
 
-    pub fn to_string(&self) -> String {
+    pub fn selection_sort_desc(&mut self) {
+        let end = self.list.len();
+        for start in 0..end {
+            let mut location_of_biggest = start;
+            for i in start..end {
+                if self.list[i] > self.list[location_of_biggest] {
+                    location_of_biggest = i;
+                }
+            }
+
+            let temp = self.list[start];
+            self.list[start] = self.list[location_of_biggest];
+            self.list[location_of_biggest] = temp;
+        }
+    }
+
+    pub fn list_items(&self) -> String {
         let mut x = String::new();
 
-        for i in 0..self.m_array.len() {
-            x += format!("{} ", self.m_array[i as usize]).as_str();
+        for i in 0..self.list.len() {
+            x += format!("{} ", self.list[i as usize]).as_str();
         }
         x
     }
